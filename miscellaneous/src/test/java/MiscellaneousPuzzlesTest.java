902f31dbfcf7044c951865ed22395d5f43744b28
Adarsh <adarsh.kumar.jobs@gmail.com>
2017-04-06 12:57:38 +0400

    Added Towers of hanoi solution

--- miscellaneous/src/test/java/MiscellaneousPuzzlesTest.java
+++ miscellaneous/src/test/java/MiscellaneousPuzzlesTest.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by adarsh on 31/03/2017.
  */
@@ -21,7 +26,7 @@ public class MiscellaneousPuzzlesTest {
             }
         }
         System.out.println(eggDrop(2, 100));
-        System.out.println(eggDrop(3,100));
+        System.out.println(eggDrop(3, 100));
     }
 
     private static int[][] cache = new int[5][200];
@@ -54,16 +59,18 @@ public class MiscellaneousPuzzlesTest {
         return min + 1;
     }
 
+    /**
+     * There are 100 closed lockers in a hallway.
+     * A man begins by opening all 100 lockers.
+     * Next, he closes every second locker.
+     * Then, on his third pass, he toggles every third locker (closes it if it is open or opens it if it is closed).
+     * This process continues for 100 passes, such that on each pass i, the man toggles every i th locker.
+     * After his 100th pass in the hallway, in which he toggles only locker #100, how many lockers are open?
+     */
+
     @Test
     public void testLockerClosingProblem() {
-        /**
-         * There are 100 closed lockers in a hallway.
-         * A man begins by opening all 100 lockers.
-         * Next, he closes every second locker.
-         * Then, on his third pass, he toggles every third locker (closes it if it is open or opens it if it is closed).
-         * This process continues for 100 passes, such that on each pass i, the man toggles every i th locker.
-         * After his 100th pass in the hallway, in which he toggles only locker #100, how many lockers are open?
-         */
+
 
         boolean[] lockers = new boolean[101];
         for (int i = 1; i <= 100; i++) {
@@ -78,4 +85,223 @@ public class MiscellaneousPuzzlesTest {
                 System.out.println(i);
         }
     }
+
+
+    //Tries to generate another configuration by moving topmost disk from tower-i to tower-j if legal
+    private List<Stack<Integer>> getNextConfiguration(List<Stack<Integer>> towers, int i, int j) {
+
+        Stack<Integer> sourceStack = towers.get(i);
+        Stack<Integer> targetStack = towers.get(j);
+
+
+        if (sourceStack.isEmpty())
+            return null;
+        if (targetStack.isEmpty() || targetStack.peek() > sourceStack.peek()) {
+            towers = clone(towers);
+            sourceStack = towers.get(i);
+            targetStack = towers.get(j);
+            targetStack.push(sourceStack.pop());
+            return towers;
+        }
+        return null;
+
+    }
+
+    private List<Stack<Integer>> clone(List<Stack<Integer>> towers) {
+        List<Stack<Integer>> clone = new ArrayList<>(towers);
+        clone.set(0, clone(clone.get(0)));
+        clone.set(1, clone(clone.get(1)));
+        clone.set(2, clone(clone.get(2)));
+        return clone;
+    }
+
+    private Stack<Integer> clone(Stack<Integer> stack) {
+        Stack<Integer> clone = new Stack<>();
+        stack.stream().forEach(clone::push);
+        return clone;
+    }
+
+    private class HanoiDTO {
+        String targetConfiguration;
+        boolean solutionFound;
+        Set<String> sourceProcessedConfigurations = new HashSet<>();
+        Set<String> targetProcessedConfigurations = new HashSet<>();
+        int numTowers;
+        int ctr;
+        Set<String> processedConfigurations = new HashSet<>();
+
+        public HanoiDTO(int numTowers) {
+            this.numTowers = numTowers;
+        }
+
+        public HanoiDTO(String targetConfiguration,int numTowers) {
+            this.targetConfiguration = targetConfiguration;
+            this.numTowers=numTowers;
+        }
+    }
+
+
+    /**
+     * In the classic problem of the Towers of Hanoi, you have 3 towers
+     * and N disks of different sizes which can slide onto any tower.
+     * The puzzle starts with disks sorted in ascending order of size from top to bottom
+     * (ie., each disk sits on top of an even larger one).
+     * You have the following constraints:
+     * (1) Only one disk can be moved at a time.
+     * (2) A disk is slid off the top of one tower onto another tower.
+     * (3) A disk cannot be placed on top of a smaller disk.
+     * Write a program to move the disks from the first tower to the last using stacks.
+     */
+    @Test
+    public void testHanoiTowers() {
+
+
+        int numDisk = 3;
+        int numTowers = 3;
+
+        for (numDisk = 1; numDisk <= 11; numDisk++) {
+            List<Stack<Integer>> sourceTowers = new ArrayList<>();
+            List<Stack<Integer>> targetTowers = new ArrayList<>();
+
+            IntStream.range(0, numTowers).boxed().forEach(i -> {
+                sourceTowers.add(new Stack<>());
+                targetTowers.add(new Stack<>());
+            });
+
+            int buff = numDisk;
+            while (buff > 0) {
+                sourceTowers.get(0).push(buff);
+                targetTowers.get(numTowers - 1).push(buff--);
+            }
+
+            HanoiDTO dto = new HanoiDTO(numTowers);
+            getSolution(sourceTowers, targetTowers,dto);
+            assertTrue(dto.solutionFound);
+            System.out.println(numDisk + "\t" + dto.ctr); //ctr keeps track of number of executions. used to estimate O(n)
+        }
+
+
+    }
+
+    private void getSolution2(List<Stack<Integer>> towers, HanoiDTO dto) {
+        dto.ctr++;
+        dto.processedConfigurations.add(stringify2(towers));
+        List<List<Stack<Integer>>> nextConfigurations = getNextConfigurations(towers, dto);
+
+        for (List<Stack<Integer>> nextConfiguration : nextConfigurations) {
+                if (stringify2(nextConfiguration).equals(dto.targetConfiguration)){
+                    dto.solutionFound = true;
+                    return;
+                }
+                getSolution2(nextConfiguration, dto);
+                if (dto.solutionFound)
+                    return;
+        }
+
+    }
+
+    private void getSolution(List<Stack<Integer>> sourceTowers, List<Stack<Integer>> targetTowers, HanoiDTO dto) {
+//        System.out.println("Source : "+stringify2(sourceTowers));
+//        System.out.println("Target : "+stringify2(targetTowers));
+        dto.ctr++;
+        dto.sourceProcessedConfigurations.add(stringify2(sourceTowers));
+        dto.targetProcessedConfigurations.add(stringify2(targetTowers));
+
+        List<List<Stack<Integer>>> nextForSource = getNextConfigurations(sourceTowers, dto, true);
+        if (dto.solutionFound)
+            return;
+        List<List<Stack<Integer>>> nextForTarget = getNextConfigurations(targetTowers, dto, false);
+        if (dto.solutionFound)
+            return;
+
+        for (List<Stack<Integer>> sourceNext : nextForSource) {
+            for (List<Stack<Integer>> targetNext : nextForTarget) {
+                if (stringify2(sourceNext).equals(stringify2(targetNext))) {
+                    dto.solutionFound = true;
+                    return;
+                }
+                getSolution(sourceNext, targetNext, dto);
+                if (dto.solutionFound)
+                    return;
+            }
+        }
+
+
+    }
+
+
+    private List<List<Stack<Integer>>> getNextConfigurations(List<Stack<Integer>> towers, HanoiDTO dto) {
+        List<List<Stack<Integer>>> listOfTowers = new ArrayList<>();
+
+            for (int i = 0; i < dto.numTowers; i++) {
+                for (int j = dto.numTowers - 1; j >= 0; j--) {
+
+                    if (i == j)
+                        continue;
+
+                    List<Stack<Integer>> nextConfiguration = getNextConfiguration(towers, i, j);
+                    if (nextConfiguration != null && !dto.processedConfigurations.contains(stringify2(nextConfiguration)))
+                        listOfTowers.add(nextConfiguration);
+
+            }
+        }
+        return listOfTowers;
+    }
+
+    private List<List<Stack<Integer>>> getNextConfigurations(List<Stack<Integer>> towers, HanoiDTO dto, boolean isSource) {
+        List<List<Stack<Integer>>> listOfTowers = new ArrayList<>();
+
+        if (isSource) {
+            for (int i = 0; i < dto.numTowers; i++) {
+                for (int j = dto.numTowers - 1; j >= 0; j--) {
+
+                    if (i == j)
+                        continue;
+                    List<Stack<Integer>> nextConfiguration = getNextConfiguration(towers, i, j);
+                    if (nextConfiguration != null && !dto.sourceProcessedConfigurations.contains(stringify2(nextConfiguration)))
+                        listOfTowers.add(nextConfiguration);
+                    if (nextConfiguration != null && dto.targetProcessedConfigurations.contains(stringify2(nextConfiguration))) {
+                        dto.solutionFound = true;
+                        return listOfTowers;
+                    }
+
+                }
+            }
+        } else {
+
+            for (int i = dto.numTowers - 1; i >= 0; i--) {
+                for (int j = 0; j < dto.numTowers; j++) {
+                    if (i == j)
+                        continue;
+                    List<Stack<Integer>> nextConfiguration = getNextConfiguration(towers, i, j);
+
+                    if (nextConfiguration != null && !dto.targetProcessedConfigurations.contains(stringify2(nextConfiguration)))
+                        listOfTowers.add(nextConfiguration);
+                    if (nextConfiguration != null && dto.sourceProcessedConfigurations.contains(stringify2(nextConfiguration))) {
+                        dto.solutionFound = true;
+                        return listOfTowers;
+                    }
+
+                }
+            }
+        }
+        return listOfTowers;
+    }
+
+    private String stringify2(List<Stack<Integer>> towers) {
+        StringBuilder sb = new StringBuilder();
+        int towerIndex = 0;
+        for (Stack<Integer> tower : towers) {
+            if (tower.isEmpty()) {
+                sb.append("_");
+            } else {
+                tower.forEach(sb::append);
+            }
+            if (towerIndex++ != 2) {
+                sb.append(";");
+            }
+        }
+        return sb.toString();
+    }
+
 }
--- miscellaneous/src/test/java/OOPTest.java
+++ miscellaneous/src/test/java/OOPTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -5,12 +6,121 @@ import org.junit.Test;
  */
 public class OOPTest {
 
+    /**
+     * Given a R x C grid with obstacles and a robot that can only move right and down,
+     * calculate shortest path from top left to bottom right
+     */
     @Test
-    public void testMineSweeper()
-    {
-MineSweeper mineSweeper = new MineSweeper();
-//        mineSweeper.printTest();
-        mineSweeper.printProper();
+    public void testRobotInGrid() {
+        int rows = 5, columns = 5;
 
+        boolean[][] grid = new boolean[rows][columns];
+        grid[0][1] = true;
+        grid[2][1] = true;
+        grid[0][3] = true;
+        grid[4][0] = true;
+        grid[4][2] = true;
+        grid[3][3] = true;
+//        printGrid(grid);
+
+        int[][] solution = new int[rows][columns]; //keeps track of min number of ways to reach a given node
+        for (int i = 0; i < solution.length; i++) {
+            for (int j = 0; j < solution[0].length; j++) {
+                solution[i][j] = -1;
+            }
+        }
+        solution[0][0] = 0;
+
+//        printSolution(solution);
+
+
+        int numWays = solve(grid, rows - 1, columns - 1, solution);
+        System.out.println(numWays);
+
+//        printSolution(solution);
     }
+
+    private int solve(boolean[][] grid, int r, int c, int[][] solution) {
+        if (r < 0 || c < 0)
+            return Integer.MAX_VALUE;
+        if (solution[r][c] != -1)
+            return solution[r][c];
+        if (grid[r][c]) {
+            solution[r][c] = Integer.MAX_VALUE;
+            return solution[r][c];
+        }
+        int solutionFromTop = solve(grid, r - 1, c, solution);
+        int solutionFromLeft = solve(grid, r, c - 1, solution);
+        if (solutionFromLeft == Integer.MAX_VALUE && solutionFromTop == Integer.MAX_VALUE) {
+            solution[r][c] = Integer.MAX_VALUE;
+            return solution[r][c];
+        }
+        solution[r][c] = Math.min(solutionFromLeft, solutionFromTop) + 1;
+        return solution[r][c];
+    }
+
+
+    private void printGrid(boolean[][] grid) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                sb.append(grid[i][j] ? 'T' : 'F');
+                sb.append("\t");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private void printSolution(int[][] solution) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < solution.length; i++) {
+            for (int j = 0; j < solution[0].length; j++) {
+                sb.append((solution[i][j] == Integer.MAX_VALUE) ? "H" : solution[i][j]);
+                sb.append("\t");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    /**
+     * A child can climb 1, 2 or 3 steps at a time. Calculate number of ways of reaching nth step
+     */
+    @Test
+    public void testNumSteps() {
+        /**
+         * Solution f(n) = f(n-1)+f(n-2)+f(n-3)
+         * f(0)=0
+         * f(1)=1
+         * f(2)=1+1=2 [2,1->1]
+         * f(3)=4 [3,1->2,2->1,1->1->1]
+         * f(4)=7 [1->3,  2->2,1->1->2,  3->1,1->2->1,2->1->1,1->1->1->1]
+         */
+
+        Assert.assertEquals(7, countNumWays(4));
+//        System.out.println(countNumWays(5));
+    }
+
+    /**
+     * Solution f(n) = f(n-1)+f(n-2)+f(n-3)
+     * f(0)=0
+     * f(1)=1
+     * f(2)=1+1=2 [2,1->1]
+     * f(3)=4 [3,1->2,2->1,1->1->1]
+     * f(4)=7 [1->3,  2->2,1->1->2,  3->1,1->2->1,2->1->1,1->1->1->1]
+     */
+    private int countNumWays(int n) {
+        int a = 1, b = 2, c = 4;
+        int d;
+        while (n-- > 3) {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+
+
 }
